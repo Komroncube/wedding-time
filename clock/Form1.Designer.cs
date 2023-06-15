037791b91f2d7d7f34131286a6d5e81610ceb3fc
Komron <komronabdujabbor@gmail.com>
2023-06-15 18:00:58 +0500

    Добавьте файлы проекта.

---
+++ clock/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            time_box = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // time_box
+            // 
+            time_box.AutoSize = true;
+            time_box.Font = new Font("Segoe UI", 40F, FontStyle.Regular, GraphicsUnit.Point);
+            time_box.Location = new Point(12, 24);
+            time_box.Name = "time_box";
+            time_box.Size = new Size(337, 106);
+            time_box.TabIndex = 0;
+            time_box.Text = "00:00:00";
+            time_box.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(358, 172);
+            Controls.Add(time_box);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Digital clock";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label time_box;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ wedding/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace wedding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public class Person
+        {
+            public Person(string name):this(name, false)
+            {
+                Name= name;
+            }
+            public Person(string name, bool uylandimi)
+            {
+                Name= name;
+                IsMarried= uylandimi;
+            }
+            public Person(string name, bool isMarried, Person pair)
+            {
+                Name= name;
+                IsMarried= isMarried;
+                if (isMarried)
+                {
+                    Pair = pair;
+                }
+                
+            }
+            public void Love(Person second)
+            {
+                if (this.IsMarried==false && second.IsMarried==false)
+                {
+                    this.IsMarried = true;
+                    this.Pair = second;
+                    second.IsMarried = true;
+                    second.Pair = this;
+                }
+                
+            }
+            public static void Marriage(Person p1, Person p2)
+            {
+                if (p1.IsMarried==false && p2.IsMarried==false)
+                {
+                    Console.WriteLine("Just Married");
+                    p1.IsMarried = true;
+                    p2.IsMarried = true;
+                    p1.Pair = p2;
+                    p2.Pair = p1;
+                }
+                else
+                {
+                    Console.WriteLine("Someone is cheating");
+                }    
+            }
+
+            public string Name { get; set; }
+            public bool IsMarried { get; set; }
+            public Person Pair { get; set; }
+                
+        }
+    }
+}
